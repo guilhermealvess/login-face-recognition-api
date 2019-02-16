587992c4fc8b536fa4223417c8e818bd8f8cddfb
Guilherme Alves <guilherme.alves3340@gmail.com>
2019-02-16 17:42:46 -0200

    Update credentials

---
+++ credentials.py
@@ -0,0 +1,10 @@
+"""
+    This file stores credential data for connection to the database
+"""
+
+mongodb = {
+    'user': 'root',
+    'password': '*******',
+    'port': 27017,
+    'host': 'localhost'
+}
